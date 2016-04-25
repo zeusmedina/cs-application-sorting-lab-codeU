cf712c7c20f1712cfe308c6dbb7a4c61766d3b9d
Allen Downey <downey@allendowney.com>
2016-04-25 14:49:08 -0400

    Removing solutions

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -63,62 +63,8 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
-		int size = list.size();
-		if (size <= 1) {
-			return list;
-		}
-		// make two lists with half the elements each.
-		List<T> first = mergeSort(new LinkedList<T>(list.subList(0, size/2)), comparator);
-		List<T> second = mergeSort(new LinkedList<T>(list.subList(size/2, size)), comparator);
-		
-		return merge(first, second, comparator);
-	}
-
-	/**
-	 * Merges two sorted lists into a single sorted list.
-	 * 
-	 * @param first
-	 * @param second
-	 * @param comparator
-	 * @return
-	 */
-	private List<T> merge(List<T> first, List<T> second, Comparator<T> comparator) {
-		// NOTE: using LinkedList is important because we need to 
-		// remove from the beginning in constant time
-		List<T> result = new LinkedList<T>();
-		int total = first.size() + second.size();
-		for (int i=0; i<total; i++) {
-			List<T> winner = pickWinner(first, second, comparator);
-			result.add(winner.remove(0));
-		}
-		return result;
-	}
-
-	/**
-	 * Returns the list with the smaller first element, according to `comparator`.
-	 * 
-	 * If either list is empty, `pickWinner` returns the other.
-	 * 
-	 * @param first
-	 * @param second
-	 * @param comparator
-	 * @return
-	 */
-	private List<T> pickWinner(List<T> first, List<T> second, Comparator<T> comparator) {
-		if (first.size() == 0) {
-			return second;
-		}
-		if (second.size() == 0) {
-			return first;
-		}
-		int res = comparator.compare(first.get(0), second.get(0));
-		if (res < 0) {
-			return first;
-		}
-		if (res > 0) {
-			return second;
-		}
-		return first;
+        // FILL THIS IN!
+        return null;
 	}
 
 	/**
@@ -129,12 +75,7 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-		PriorityQueue<T> heap = new PriorityQueue<T>(list.size(), comparator);
-		heap.addAll(list);
-		list.clear();
-		while (!heap.isEmpty()) {
-			list.add(heap.poll());
-		}
+        // FILL THIS IN!
 	}
 
 	
@@ -148,23 +89,8 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-		PriorityQueue<T> heap = new PriorityQueue<T>(list.size(), comparator);
-		for (T element: list) {
-			if (heap.size() < k) {
-				heap.offer(element);
-				continue;
-			}
-			int cmp = comparator.compare(element, heap.peek());
-			if (cmp > 0) {
-				heap.poll();
-				heap.offer(element);
-			}
-		}
-		List<T> res = new ArrayList<T>();
-		while (!heap.isEmpty()) {
-			res.add(heap.poll());
-		}
-		return res;
+        // FILL THIS IN!
+        return null;
 	}
 
 	
