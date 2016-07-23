883bc93f14a4e9ffbd730c45e906185b0ce570c9
zeusmedina <zeusmedina2@gmail.com>
2016-07-23 16:50:35 -0700

    Done.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -64,7 +64,44 @@ public class ListSorter<T> {
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+        
+    int size = list.size();
+		if (size <= 1) {
+			return list;
+		}
+		// make two lists with half the elements each.
+		List<T> first = mergeSort(new LinkedList<T>(list.subList(0, size/2)), comparator);
+		List<T> second = mergeSort(new LinkedList<T>(list.subList(size/2, size)), comparator);
+		
+		return merge(first, second, comparator);
+	}
+
+		private List<T> merge(List<T> first, List<T> second, Comparator<T> comparator) {
+		// NOTE: using LinkedList is important because we need to 
+		// remove from the beginning in constant time
+		List<T> result = new LinkedList<T>();
+		int total = first.size() + second.size();
+		for (int i=0; i<total; i++) {
+			List<T> winner = pickWinner(first, second, comparator);
+			result.add(winner.remove(0));
+		}
+		return result;
+	}
+		private List<T> pickWinner(List<T> first, List<T> second, Comparator<T> comparator) {
+		if (first.size() == 0) {
+			return second;
+		}
+		if (second.size() == 0) {
+			return first;
+		}
+		int res = comparator.compare(first.get(0), second.get(0));
+		if (res < 0) {
+			return first;
+		}
+		if (res > 0) {
+			return second;
+		}
+		return first;
 	}
 
 	/**
@@ -75,7 +112,12 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+     PriorityQueue<T> heap = new PriorityQueue<T>(list.size(), comparator);
+		heap.addAll(list);
+		list.clear();
+		while (!heap.isEmpty()) {
+			list.add(heap.poll());
+		}
 	}
 
 	
@@ -90,7 +132,23 @@ public class ListSorter<T> {
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+    PriorityQueue<T> heap = new PriorityQueue<T>(list.size(), comparator);
+		for (T element: list) {
+			if (heap.size() < k) {
+				heap.offer(element);
+				continue;
+			}
+			int cmp = comparator.compare(element, heap.peek());
+			if (cmp > 0) {
+				heap.poll();
+				heap.offer(element);
+			}
+		}
+		List<T> res = new ArrayList<T>();
+		while (!heap.isEmpty()) {
+			res.add(heap.poll());
+		}
+		return res;
 	}
 
 	
